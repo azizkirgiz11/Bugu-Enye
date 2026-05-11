76dd163f6493306f1480e31209fa6dcc273e52f4
azizkirgiz11 <azizkirgiz11@gmail.com>
2026-05-11 08:11:25 +0600

    fixed buy ticket and daily routin

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import Footer from './components/Footer'
 import Tickets from './pages/Tickets'
 import About from './pages/About'
 import Home from './pages/Home';
+import BuyTicket from './pages/BuyTicket';
 
 const App = () => {
   return (
@@ -15,6 +16,7 @@ const App = () => {
         <Route path="/" element={<Home/>} />
         <Route path="/about" element={<About/>} />
         <Route path="/tickets" element={<Tickets/>} />
+        <Route path="/buy-ticket" element={<BuyTicket/>} />
       </Routes>
       <Footer/>
     </div>
